b4716af183c7b54ecaaf3d93aa357d5f57b15844
krishna <krishna.thallavarjalla@infomagnus.com>
2021-02-09 19:22:19 +0530

    Angular Library

---
+++ projects/components-lib/src/lib/components-lib.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cmpts-ComponentsLib',
+  template: ` <p>Hello Infomagnus Library</p> `,
+  styles: [],
+})
+export class ComponentsLibComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
